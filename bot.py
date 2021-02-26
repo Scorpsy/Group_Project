3bc37416a7a629ee58ca8d1e802924478cba9ee6
Scorpsy <matthewraynard65@gmail.com>
2021-02-26 02:39:03 -0800

    added searching for r/stocks and r/investing

--- bot.py
+++ bot.py
@@ -1,13 +1,70 @@
 #!/usr/bin/env python3
+import praw
+import os
+import time
+import datetime as dt
+from os.path import join,dirname
+from dotenv import load_dotenv
+
 
 
 def main() -> None:
 
-    print('gay')
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    reddit = praw.Reddit(client_id = os.getenv('client_id'),
+                        client_secret =  os.getenv('client_secret'),
+                        username =  os.getenv('username'),
+                        password = os.getenv('password'),
+                        user_agent = os.getenv('user_agent'))
+
+    investing = reddit.subreddit('investing')
+    hot_investing = investing.hot(limit=5)
+
+    print("r/investin on TSLA\n")
+    investing_search = investing.search(query='TSLA', time_filter = 'week')
+    for submission in investing_search:
+        if not submission.stickied:
+            time = submission.created_utc
+            utc_time = get_date(time)
+
+            print(utc_time)
+            print(submission.title)
+
+    print("\n")
+
+    print("Investing Subreddit\n")
+
+    for submission in hot_investing:
+        if not submission.stickied:
+            print('Title: {}'.format(submission.title))
+
+    stocks = reddit.subreddit('stocks')
+    hot_stocks = stocks.hot(limit=5)
+
+    print("\n")
+
+    print("r/stocks on MSFT\n")
+
+    stocks_search = stocks.search(query = 'MSFT', time_filter = 'week')
+    for submission in stocks_search:
+        if not submission.stickied:
+            time = submission.created_utc
+            utc_time = get_date(time)
+            print(submission.title)
+
+    print("\n")
+
+    print("Stocks Subreddit\n")
+
+    for submission in hot_stocks:
+        if not submission.stickied:
+            print('Title: {}'.format(submission.title))
 
 
-
-
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
 
 if __name__ == "__main__":
     main()
