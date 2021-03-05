7694fb9e4f0b6e006f02043171d461d2cdc15f9f
Scorpsy <matthewraynard65@gmail.com>
2021-03-04 19:41:14 -0800

    added time

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from os.path import join,dirname
 from dotenv import load_dotenv
 
 
-
 def main() -> None:
 
     dotenv_path = join(dirname(__file__), '.env')
@@ -23,7 +22,37 @@ def main() -> None:
     hot_investing = investing.hot(limit=5)
 
     print("r/investin on TSLA\n")
-    investing_search = investing.search(query='TSLA', time_filter = 'week')
+    investing_search = investing.search(query='TSLA', time_filter = 'all')
+    #print(size(investing_search))
+    for submission in investing_search:
+        if not submission.stickied:
+            time = submission.created_utc
+            utc_time = get_date(time)
+
+            print(utc_time)
+            print(submission.title)
+            #print(submission.ups)
+            #print(submission.downs)
+
+
+
+    print("r/investin on MSFT\n")
+    investing_search = investing.search(query='MSFT', time_filter = 'all', limit = 5)
+    #print(size(investing_search))
+    for submission in investing_search:
+        if not submission.stickied:
+            time = submission.created_utc
+            utc_time = get_date(time)
+
+            print(utc_time)
+            print(submission.title)
+            print(submission.ups)
+            print(submission.upvote_ratio)
+
+
+    print("r/investin on GOOGL\n")
+    investing_search = investing.search(query='GOOGL', time_filter = 'all', limit = 5)
+    #print(size(investing_search))
     for submission in investing_search:
         if not submission.stickied:
             time = submission.created_utc
@@ -32,36 +61,42 @@ def main() -> None:
             print(utc_time)
             print(submission.title)
 
+
+
+
+
     print("\n")
 
     print("Investing Subreddit\n")
 
-    for submission in hot_investing:
-        if not submission.stickied:
-            print('Title: {}'.format(submission.title))
+    #for submission in hot_investing:
+    #    if not submission.stickied:
+    #        print('Title: {}'.format(submission.title))
 
     stocks = reddit.subreddit('stocks')
     hot_stocks = stocks.hot(limit=5)
 
-    print("\n")
+    #print("\n")
 
-    print("r/stocks on MSFT\n")
+    #print("r/stocks on MSFT\n")
+    
+    #stocks_search = stocks.search(query = 'MSFT', time_filter = 'week')
+    #for submission in stocks_search:
+    #    if not submission.stickied:
+    #        time = submission.created_utc
+    #        utc_time = get_date(time)
 
-    stocks_search = stocks.search(query = 'MSFT', time_filter = 'week')
-    for submission in stocks_search:
-        if not submission.stickied:
-            time = submission.created_utc
-            utc_time = get_date(time)
-            print(submission.title)
+    #        print(utc_time)
+    #        print(submission.title)
 
-    print("\n")
+    #print("\n")
 
-    print("Stocks Subreddit\n")
+    #print("Stocks Subreddit\n")
 
-    for submission in hot_stocks:
-        if not submission.stickied:
-            print('Title: {}'.format(submission.title))
-
+    #for submission in hot_stocks:
+    #    if not submission.stickied:
+    #        print('Title: {}'.format(submission.title))
+    
 
 def get_date(created):
     return dt.datetime.fromtimestamp(created)
